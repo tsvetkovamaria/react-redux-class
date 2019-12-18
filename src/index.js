ab6a9a1f38eaed911f18b0cfec6a4392ed5eaa9c
Maria Gudova <gudova.m@chocofood.kz>
2019-12-18 20:38:22 +0600

    try find a bug with react and redux store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider }  from 'react-redux';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools} from 'redux-devtools-extension';
+import axios from 'axios';
+
 import App from './App';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// -------------------- Настройки, подготовка ---------------
+
+const defaultState = {
+    menu: "some str"
+};
+
+const FETCH_MENU_SUCCESS = 'FETCH_MENU_SUCCESS'
+const menuReducer = (state = defaultState, action) => {
+    switch (action.type) {
+        case FETCH_MENU_SUCCESS:
+            state.menu = action.menu;
+            return state;
+        default:
+            return state;
+    }
+};
+
+const store = createStore(menuReducer);
+
+// -------------------- Отсюда идет работа с данными --------------------
+
+const actionFetchMenu = (menu) => ({
+    type: 'FETCH_MENU_SUCCESS',
+    menu: menu
+});
+
+window.actionFetchMenu = actionFetchMenu;
+
+const fetchMenu = async () => {
+    const response = await axios.get('https://test-461bf.firebaseio.com/menu.json/');
+    return response.data;
+}
+
+// fetchMenu().then(menu => store.dispatch(actionFetchMenu(menu)))
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+window.store = store;
